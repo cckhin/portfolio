09d1b2728dab229b35b4185353ba12a3b090422a
khinkyikyikhine <khinkyikyikhine253@gmail.com>
2021-08-25 11:24:58 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(function() {
+  console.log( "ready!" );
+  AOS.init();
+});
+
+function scrollToTargetElement(elementId) {
+  $('html, body').animate({
+    scrollTop: $(`#${elementId}`).offset().top
+  }, 500);
+}
+
+// jQuery(document).ready(function() {
+//   jQuery('.skillbar').each(function() {
+//       jQuery(this).find('.skillbar-bar').animate({ width: jQuery(this).attr('data-percent') }, 3000);
+//   });
+// });
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
