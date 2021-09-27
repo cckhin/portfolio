9a33970c97a3771212e91d74026242e77390a0ea
khinkyikyikhine <khinkyikyikhine253@gmail.com>
2021-09-27 16:54:26 +0900

    correct data

--- js/init.js
+++ js/init.js
@@ -2,6 +2,20 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $('#nav-mobile li').click(function(){
+      $('#nav-mobile').hide();
+      console.log("OK test");
+    });
+    $('.sidenav-trigger').click(function(){
+      $('#nav-mobile').show();
+        //  $(document.body).click( function() {
+        //   $('#nav-mobile').hide();
+        // });
+    });
+
+    $(document.section).click( function() {
+      $('#nav-mobile').hide();
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
